1e71bd336695c838bb4182c89c3a6d30157b5a9b
Alexdxe <106116730+Alexdxe@users.noreply.github.com>
2025-08-12 15:32:58 -0700

    Add files via upload

---
+++ pages/Links.py
@@ -0,0 +1,26 @@
+#Import necessary libraries
+import streamlit as st
+from PIL import Image
+
+st.header("Links")
+
+#LinkedIn
+st.subheader("LinkedIn")
+st.image("images/LinkedIn-Logo.wine.png", width = 500)
+st.markdown("Feel free to connect with me on LinkedIn!")
+st.link_button("My LinkedIn Profile", "https://www.linkedin.com/in/alexander-cai-8b1694365/", type="primary")
+st.write("")
+
+#GitHub
+st.subheader("GitHub")
+st.image("images/maxresdefault.jpg", width = 500)
+st.markdown("Visit my GitHub to see all of the code for my projects.")
+st.link_button("My LinkedIn Profile", "https://github.com/Alexdxe", type="primary")
+st.write("")
+
+#Photography Account
+st.subheader("Photography")
+st.image("images/Instagram_icon.png",  width = 250)
+st.markdown("Check out my photography account on Instagram. Feel free to message me if you ever need photos.")
+st.link_button("My Photography Account", "https://www.instagram.com/fluid_photoss/", type="primary")
+st.write("")
