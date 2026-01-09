b09644d61e2b5feb7c3deec9d3b63820a7e96740
Alex Cai <106116730+Alexdxe@users.noreply.github.com>
2026-01-08 16:22:51 -0800

    Add files via upload

--- Profile.py
+++ Profile.py
@@ -5,6 +5,7 @@ from PIL import Image
 #Title and header for the profile page
 st.markdown("<h1 style='text-align: center;'>Alexander Cai</h1>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center;'>Welcome to my profile page!</h5>", unsafe_allow_html=True)
+st.markdown("<h7 style='text-align: center;'>Last Updated: 01/08/2026</h7>", unsafe_allow_html=True)
 
 
 #Profile Photo:
@@ -33,7 +34,10 @@ classes = [
     ("STA 108", "Regression Analysis"),
     ("MAT 022A", "Linear Algebra"),
     ("STA 141A", "Statistical Data Science"),
-    ("ECS 116", "Databases for Non-Majors")
+    ("ECS 116", "Databases for Non-Majors"),
+    ("ECS 119", "Data Processing Pipelines"),
+    ("STS 110", "Computing, Data, and Law in the United States"),
+    ("STA 131A", "Intro to Probability Theory")
 ]
 prefix = {
     "Statistics": "STA",
@@ -65,18 +69,17 @@ st.write("")
 st.subheader("What Have I Been Up To?")
 st.write("")
 st.image("images/UC-Davis-Logo.png", width = 350)
-st.markdown("**1) School:** With school back in session, I have been taking some time to adjust back into the school mindset, studying material and completing homework assignments. Classes I am taking this quarter include: **STA 131A**, **ECS 119**, **STS 110**, and **NUT10V**.")
+st.markdown("**1) School:** With school back in session, I have been taking some time to adjust back into the school mindset, studying material and completing homework assignments. Classes I am taking this quarter include: **STA 104**, **STA141B**, and **UWP104T**.")
 st.write("")
 
 st.image("images/logo-white-transparentbg.png", width = 150)
-st.markdown("**2) Internships:** I am the current Data Science Manager for Associated Students of UC Davis Pantry. My role is overseeing all data-related tasks, including data analysis, projects, and collection. Currently, I am producing monthly reports based on donations and consumer data. I am also in the process of brainstorming up a large-scale feedback-based project for me and my team to complete.")
+st.markdown("**2) Internships:** I am the current Data Science Manager for Associated Students of UC Davis Pantry. My role is overseeing all data-related tasks, including data analysis, projects, and collection. Currently, I am in the process of creating a dashboard to visualize various metrics for the pantry, and updating the tracking website to streamline data collection. ")
 st.write("")
-
 st.markdown("**3) Independent Learning:** Outside of Data Science, I have been learning and practicing the guitar, Japanese (Duolingo) and photography/photoshop. While these skills are not related to Data Science, they help me develop other skills like pattern recognition, patience, and persistance, which are all important in the field of Data Science.")
 photos = {
 "Photography": {
-    "image": Image.open("images/DSC03650.jpg"),
-    "caption": "A photo of the lighthouse at San Francisco's Fort Point, taken on a Sony A6000"
+    "image": Image.open("images/DSC06126.jpg"),
+    "caption": "A photo of Ocean Beach's evening view, taken on a Sony A6000"
 },
  
 "Guitar": { 
@@ -84,8 +87,8 @@ photos = {
     "caption": "A photo of my Scheter Demnon-6 guitar, currently I am learning the fingerstyle version of 打上花火 by DAOKO and Kenshi Yonezu."
 },
 "Duolingo": { 
-    "image": Image.open("images/Screenshot_20250807_115706_Duolingo.jpg"),
-    "caption": "A screenshot of my current Duolingo streak, I have been learning Japanese consistently for the past 2 months, practicing for about 20 minutes a day."
+    "image": Image.open("images/duo.png"),
+    "caption": "A screenshot of my Duolingo 2025 Year in Review. I have been practicing Japanese consistently for 160 days, spending roughly 12 minutes a day polishing my speaking skills."
 } 
 }
 choice = st.selectbox("Images:", list(photos.keys()))
